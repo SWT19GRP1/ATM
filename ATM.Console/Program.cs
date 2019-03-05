7614a6c60b309c20a0145fd728b81ce50604b404
Simon Wessmann Brok <au586362@uni.au.dk>
2019-03-05 14:05:03 +0100

    Tranponderreciever opdateret med Attach, detach. Der kan nu subscribes på eventet.

--- ATM.Console/Program.cs
+++ ATM.Console/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace ATM.Console
 {
@@ -10,7 +12,13 @@ namespace ATM.Console
     {
         private static void Main(string[] args)
         {
-             
+            var reciever = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            var transponderReciever = new TransponderReciever(reciever);
+
+            while (true)
+            {
+                
+            }
         }
     }
 }
---
+++ Someclassidelete/TransponderReciever.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TransponderReceiver;
+
+namespace ATM
+{
+    public class TransponderReciever
+    {
+        private ITransponderReceiver _receiver;
+
+        public TransponderReciever(ITransponderReceiver receiver)
+        {
+            _receiver = receiver;
+            Attach();
+        }
+
+        public void Attach()
+        {            
+            _receiver.TransponderDataReady += RecieverOnTransportDataReady;
+        }
+
+        public void Detach()
+        {
+            _receiver.TransponderDataReady -= RecieverOnTransportDataReady;
+        }
+        private static void RecieverOnTransportDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                Console.WriteLine($"Transponderdata {data}");
+            }
+        }
+    }
+}
